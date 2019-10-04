f67f827bbe707e11bd342b611833fdf3d53c6542
Heorhii Siburov <g.siburov@gmail.com>
2019-10-04 08:13:55 -0700

    DB imported

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const server = express();
 
-const port = 5000
+const projectDB = require('./data/helpers/projectModel');
+const actionDB = require('./data/helpers/actionModel.js');
 
+// Test request
 server.get('/', (req, res) => {
     res.send(`
-      <h2>Heorhii API</h>
+      <h2>Heorhii Sprint API</h>
       <p>Welcome to the Heorhii API</p>
     `);
 });
+
+const port = 5000
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
