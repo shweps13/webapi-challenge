6426df5792574327829d52d69f9e79d3e3fed823
Heorhii Siburov <g.siburov@gmail.com>
2019-10-04 13:32:44 -0700

    reformatted, PUT feature working

--- backend-test/src/components/MainBlock.js
+++ backend-test/src/components/MainBlock.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Message } from 'semantic-ui-react'
+import { Card, Message, Button, Form } from 'semantic-ui-react'
 
 import AxiosThing from '../utilites/AxiosThing';
-import CardBtn from './CardBtn';
 import AddProject from './AddProject';
+import ProjectCards from './ProjectCards'
 
+const initialItem = {
+    name: '',
+    description: '',
+    completed: false
+  };
 
 function MainBlock() {
 
     const [projects, setProjects] = useState([]);
     const [state, setState] = useState('');
     const [update, setUpdate] = useState(false);
-    console.log(projects);
+    const [editing, setEditing] = useState(false);
+    const [itemToEdit, setItemToEdit] = useState(initialItem);
+    const [edited, setEdited] = useState('');
+    // console.log(editing);
 
     useEffect(() => {
         AxiosThing()
@@ -20,31 +28,26 @@ function MainBlock() {
             setProjects(res.data);
         })
         .catch(err => console.log(err));
-      }, [state, update]);
+      }, [state, update, edited]);
 
-    function Completed(message) {
-    if (message.completed === true) {
-        return <Message success header='Completed'/>
-    } else {
-        return <Message error header='Not completed'/>
-        }
-    }
 
   return (
     <div className="MainBlock">
         <AddProject update={update} setUpdate={setUpdate} />
         <Card.Group centered>
             {projects.map(project => (
-                        <Card key={project.id}>
-                            <Card.Content>
-                                <Card.Header>Project {project.id}</Card.Header>
-                                <Card.Meta>{project.name}</Card.Meta>
-                                <Card.Description>{project.description}</Card.Description>
-                                <Completed completed={project.completed} />
-                            </Card.Content>
-                            <CardBtn id={project.id} setState={setState} />
-                        </Card>
-                    ))}
+            <ProjectCards 
+            key={project.id} 
+            id={project.id}
+            name={project.name} 
+            description={project.description} 
+            completed={project.completed}
+            setState={setState}
+            editing={editing}
+            setEditing={setEditing}
+            itemToEdit={itemToEdit}
+            setItemToEdit={setItemToEdit}
+            setEdited={setEdited} />))}
         </Card.Group>
     </div>
   );
